e475a8149d709af020aec1569e6e06d0680a7498
LEE BUM KI <66540979+beombeom1119@users.noreply.github.com>
2022-01-27 16:03:02 +0900

    Update instargram_image.py

--- instargram_image.py
+++ instargram_image.py
@@ -74,52 +74,7 @@ for image in images:
 
 
 
-# driver.get("https://www.instagram.com/")
-# elem = driver.find_element_by_name("username")
-# elem.send_keys("gowjr1119@daum.net")
-# elem1 = driver.find_element_by_name("password")
+
+
+# elem.send_keys("gojr1119@daum.net")
 # elem1.send_keys("1119beomki")
-# # elem1.send_keys(Keys.RETURN)
-
-
-
-# #스크롤 내리는거
-# SCROLL_PAUSE_TIME = 1
-
-# # Get scroll height
-# last_height = driver.execute_script("return document.body.scrollHeight")
-
-# while True:
-#     # Scroll down to bottom
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-#     # Wait to load page
-#     time.sleep(SCROLL_PAUSE_TIME)
-
-#     # Calculate new scroll height and compare with last scroll height
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#     if new_height == last_height:
-#         try:
-#             driver.find_elements_by_css_selector(".mye4qd").click()
-#         except:
-#             break
-            
-#     last_height = new_height
-
-# #스크롤 다 내림
-
-# images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-# count =1
-# for image in images:
-#     try:
-#         image.click()
-#         time.sleep(3) 
-#         imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
-#         # driver.find_elements_by_css_selector(".n3VNCb").get_attribute("src")
-#         urllib.request.urlretrieve(imgUrl,str(count)+".jpg")
-#         count = count+1
-#     except:
-#         pass
-
-
-# driver.close()
