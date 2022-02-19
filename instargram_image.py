bd8526279bccb28565e535e4b44056b9041055d8
beombeom1119 <qjarl1119@naver.com>
2022-02-19 11:42:27 +0900

    new crawling

--- instargram_image.py
+++ instargram_image.py
@@ -3,36 +3,38 @@ from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/Users/beom/Documents/Code/crawling/chromedriver")
 driver.get("https://www.instagram.com/")
 time.sleep(2)
 
-email = 'email@email.com' 
+email = 'gowjr119@daum.net' 
 input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
 input_id.clear()
 input_id.send_keys(email)
 
-password = 'password' 
+password = '' 
 input_pw = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[1]
 input_pw.clear()
 input_pw.send_keys(password)
 input_pw.submit()
-time.sleep(7)
+time.sleep(3)
 try:
     driver.find_element_by_css_selector('button.sqdOP.yWX7d.y3zKF').click()
 except:
     print("하하")
-time.sleep(5)
+time.sleep(2)
+print("로그인 완료")
 try:
     driver.find_element_by_css_selector('button.aOOlW.HoLwm').click()
 except:
     print("하하")
-search = "남성 탈모" #검색할 키워드
+search = "#한라산정상1950m" #검색할 키워드
 time.sleep(2)
-elem = driver.find_element_by_css_selector('input.XTCLo.x3qfX')
+elem = driver.find_element_by_css_selector('input.XTCLo.d_djL.DljaH')
 elem.send_keys(search)
 time.sleep(3)
-driver.find_element_by_css_selector('div.z556c').click()
+print("검색 완료")
+driver.find_element_by_css_selector('div._7UhW9.xLCgt.qyrsm.KV-D4.uL8Hv').click()
 #스크롤 내리는거
 SCROLL_PAUSE_TIME = 1
 # # Get scroll height
@@ -41,40 +43,29 @@ timer = 1
 while (timer<5):
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
     # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
     timer=timer+1
 
 count =0
 images = driver.find_elements_by_css_selector("img.FFVAD")
-print("하하2")
 print(len(images))
-print("하하1")
+time.sleep(4)
+
 for image in images:
+    print("확인2")
     try:
-        time.sleep(3)
-        print("하하2")
+        print("확인3")
+        time.sleep(4)
+        print("확인4")
         imgUrl= driver.find_elements_by_css_selector("img.FFVAD")[count].get_attribute("src")
-        #imgUrl=image[count].get_attribute("src")
-        print("glgl")
-       # driver.find_elements_by_css_selector(".n3VNCb").get_attribute("src")
+        print("다운로드 시작")
         urllib.request.urlretrieve(imgUrl,str(count)+".jpg")
+        print("다운로드 완료")
         count = count+1
     except:
         pass
+        
     
-
-
-
-
-
-
-
-
-
-
-
-
 # elem.send_keys("gojr1119@daum.net")
 # elem1.send_keys("1119beomki")
